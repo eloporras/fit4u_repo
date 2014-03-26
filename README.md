30e1fd39a02c466e9120c3e3dc0db4609545a26d
eloporras <eloporras@gmail.com>
2014-03-26 15:20:13 -0400

    Reset the repo

--- README.md
+++
@@ -1,4 +0,0 @@
-fit4u_repo
-==========
-
-The Fit for u Repo for CEN4021
--- gen/team/nofold/version1/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package team.nofold.version1;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
